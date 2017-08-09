f11ebe8653eb019a0342053c0f7c7d55721f6e29
spkelle2 <seanpkelley94@gmail.com>
2017-08-08 20:26:01 -0500

    front end updates

--- open_route/open_route/settings.py
+++ open_route/open_route/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'j^tlrz*nx8g!s6q@mg+g+(c26&l#a#0a%i%!yt9$uf8e!atekm'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['35.163.169.135']
+ALLOWED_HOSTS = ['35.163.50.209', 'open-route.website', '127.0.0.1']
 
 
 # Application definition
